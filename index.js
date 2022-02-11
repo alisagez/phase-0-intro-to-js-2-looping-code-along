ea9cf5f36fa8d9512680e3ac2ae050630612a173
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-10 23:16:35 -0700

    commit tree b223dce95741dcfd57bf9d839c35088a00cab71f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const cardRecipient = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cardRecipient) {
+    let messages = []
+    for (let i = 0; i < cardRecipient.length; i++) {
+    messages.push(`Thank you, ${cardRecipient[i]}, for the wonderful surprise gift!`)
+    }
+return messages
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
